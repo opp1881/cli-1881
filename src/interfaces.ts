3aa6527c108c90b92c960bc7e120dea6887d2329
Mats Byrkjeland <matsbyr@gmail.com>
2020-08-25 18:37:02 +0200

    Add eslint and prettier

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,24 +1,24 @@
 export interface SearchContact {
-    type: 'Person',
-    id: string,
-    name: string,
-    postCode: string,
-    postArea: string,
+    type: 'Person'
+    id: string
+    name: string
+    postCode: string
+    postArea: string
     infoUrl: string
 }
 
 export interface ContactPoint {
-    label: string,
-    main: boolean,
-    type: 'Phone' | 'MobilePhone',
+    label: string
+    main: boolean
+    type: 'Phone' | 'MobilePhone'
     value: string
 }
 
 export interface Geography {
-    municipality: string,
-    county: string,
-    region: string,
-    coordinate: null,
+    municipality: string
+    county: string
+    region: string
+    coordinate: null
     address: {
         street: string
         houseNumber: string
@@ -30,17 +30,17 @@ export interface Geography {
 }
 
 export interface LookupContact {
-    type: 'Person',
-    id: string,
-    firstName: string,
-    lastName: string,
-    name: string,
-    geography: Geography,
-    contactPoints?: ContactPoint[],
+    type: 'Person'
+    id: string
+    firstName: string
+    lastName: string
+    name: string
+    geography: Geography
+    contactPoints?: ContactPoint[]
     infoUrl: string
 }
 
 export interface SearchResult<T> {
-    count: number,
+    count: number
     contacts: T[]
 }
